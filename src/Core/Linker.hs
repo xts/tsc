ae3991a536bf46445e15eaf267ded125bc40863f
Terje Stoeback <terje@stoeback.net>
2021-04-03 10:17:26 +0100

    Link in temporary directory.

--- src/Core/Linker.hs
+++ src/Core/Linker.hs
@@ -8,18 +8,23 @@ import Data.ByteString (ByteString)
 import Data.ByteString qualified as BS
 import Data.ByteString.UTF8 (toString)
 import Data.FileEmbed (embedFile)
+import Prelude hiding (writeFile)
 import System.Exit (ExitCode(..))
 import System.Process.Typed (readProcess, proc)
-import Prelude hiding (writeFile)
+import System.IO.Temp (withSystemTempDirectory)
 
 rts :: ByteString
 rts = $(embedFile "src/Core/rts.c")
 
 link :: ByteString -> String -> ExceptT String IO ()
 link source out = do
-  lift $ BS.writeFile "/tmp/program.s" source
-  lift $ BS.writeFile "/tmp/rts.c" rts
-  (rc, _, stderr) <- readProcess (proc "clang" ["/tmp/rts.c", "/tmp/program.s", "-o", out])
-  case rc of
-    ExitSuccess   -> pure ()
-    ExitFailure _ -> fail $ toString $ BS.toStrict stderr
+  withSystemTempDirectory "tsc.work" $ \dir -> do
+    let srcPath = dir <> "/program.s"
+        rtsPath = dir <> "/rts.c"
+    lift $ print srcPath
+    lift $ BS.writeFile srcPath source
+    lift $ BS.writeFile rtsPath rts
+    (rc, _, stderr) <- readProcess (proc "clang" [rtsPath, srcPath, "-o", out])
+    case rc of
+      ExitSuccess   -> pure ()
+      ExitFailure _ -> fail $ toString $ BS.toStrict stderr
