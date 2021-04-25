fdbaa9082f0e0d1d1e0ee2e4257f4a43c23eb674
Terje Stoeback <terje@stoeback.net>
2021-04-25 19:40:29 +0100

    Desugar recursively.

--- src/Core/Transformers/Desugarer.hs
+++ src/Core/Transformers/Desugarer.hs
@@ -11,7 +11,17 @@ desugar es = transform $ Right $ define es
 define :: [Expr] -> [Expr]
 define (FunDef name vs bs : es) =
   [Let [Binding name (LamDef vs (FreeArgs []) (define bs))] (define es)]
-define (VarDef name e : es) = let [e'] = define [e] in
-  [Let [Binding name e'] (define es)]
+define (VarDef name e : es) =
+  [Let [Binding name $ define1 e] (define es)]
+define (Let vs bs : es) =
+  Let (map (\(Binding n e) -> Binding n (define1 e)) vs) (define bs) : define es
+define (If p t f : es) =
+  If (define1 p) (define1 t) (define1 f) : define es
+define (List es : es') = List (define es) : define es'
 define (e : es) = e : define es
 define [] = []
+
+define1 :: Expr -> Expr
+define1 e = case define [e] of
+  [e'] -> e'
+  _    -> error "internal error: define1"
