0894065e279e8608651481a07c84eeca77ec7718
Terje Stoeback <terje@stoeback.net>
2021-04-27 12:49:21 +0100

    First class fixed-arity primitives by wrapping.

--- src/Core/Prelude/prelude.scm
+++ src/Core/Prelude/prelude.scm
@@ -1,18 +1,54 @@
+()
+
+;; Wrap fixed-arity primitives to make them first-class.
+(define (display x)
+  (#display x))
+
+(define (eq x y)
+  (#eq x y))
+
+(define (= x y)
+  (#= x y))
+
+(define (< x y)
+  (#< x y))
+
+(define (cons a b)
+  (#cons a b))
+
+(define (car xs)
+  (#car xs))
+
+(define (cdr xs)
+  (#cdr xs))
+
+(define (number->char k)
+  (#number->char k))
+
+(define (char->number c)
+  (#char->number c))
+
+;; Print newline.
 (define (newline)
   (display "\n"))
 
+;; Invert boolean.
 (define (not x)
   (if x #f #t))
 
+;; Greater-than.
 (define (> x y)
   (< y x))
 
+;; Less-than-or-equal.
 (define (<= x y)
   (not (< y x)))
 
+;; Greater-than-or-equal.
 (define (>= x y)
   (not (< x y)))
 
+;; Empty list predicate.
 (define (null? xs)
   (eq xs ()))
 
@@ -41,7 +77,7 @@
 
 ;; Reverse list XS.
 (define (reverse xs)
-  (fold-left (lambda (x acc) (cons x acc)) () xs))
+  (fold-left cons () xs))
 
 ;; Sort a numeric list XS with insertion sort.
 (define (sort xs)
